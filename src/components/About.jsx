1409cd91bd6400431bab34d4b1edbe0f7a6bfe03
PC <prantik007@gmail.com>
2022-04-16 14:05:08 +0530

    added about us

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,61 @@
-import { Flex, Heading } from "@chakra-ui/react";
-import React from "react";
+import {
+  Flex,
+  Heading,
+  Box,
+  Text,
+  Center,
+  VStack,
+  HStack,
+  Button,
+  useColorModeValue,
+} from '@chakra-ui/react';
+import React from 'react';
+import { FaGithub } from 'react-icons/fa';
 
 const About = () => {
-  return <Flex justify={'center'} align='center' mt={60}>
-  <Heading color={'pink.600'}>In Progress...</Heading>
-  </Flex>
+  const handleOnClickGithub = () => {
+    window.open('https://github.com/prantik007/pnotes', '_blank');
+  };
+
+  return (
+    <Flex direction={'column'} justify={'center'} align="center" mt={10}>
+      <Box d="flex" alignItems="center">
+        <Heading fontSize="7xl" color="green.400">
+          P
+        </Heading>
+        <Heading fontWeight="light" fontSize="5xl">
+          -Notes{' '}
+        </Heading>
+      </Box>
+      <VStack spacing={16} justify={'center'}>
+        <Text fontSize={'2xl'} w={500} mt={16}>
+          P-Notes is a simple note-taking website which can sync notes across
+          various devices. 🗒️
+        </Text>
+
+        <HStack>
+          <Text fontSize={'3xl'} >
+            Made by
+          </Text>
+          <Text fontSize={'3xl'} color={useColorModeValue('green.700','blue.300')}>Prantik Chakraborty</Text>
+        </HStack>
+
+        <Text fontSize={'2xl'}>
+          React JS, react-router-dom, Chakra-UI, Firebase
+        </Text>
+
+        <HStack>
+          <Button
+            onClick={handleOnClickGithub}
+            leftIcon={<FaGithub />}
+            bgColor={useColorModeValue('green.400', 'blue.600')}
+          >
+            GitHub
+          </Button>
+        </HStack>
+      </VStack>
+    </Flex>
+  );
 };
 
 export default About;
